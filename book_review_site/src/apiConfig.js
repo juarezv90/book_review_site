1ee4ba4793ac1106dbae8d2450ecfda4efe032ca
Victor Juarez <juarezv1990@gmail.com>
2024-08-06 12:19:31 -0400

    configuration links

--- book_review_site/src/apiConfig.js
+++ book_review_site/src/apiConfig.js
@@ -1,7 +1,13 @@
 const API_BASE_URL = 'http://127.0.0.1:8000';
 
+//TODO: Need to move all links from projects to this location
+
 const API_ENDPOINTS = {
-    GETADDBOOKS: `${API_BASE_URL}/api/books/`
+    GETADDBOOKS: `${API_BASE_URL}/api/books/`,
+    LOGIN:"",
+    REFRESHTOKEN:"",
+    GETSINGLEBOOK:"",
+    GETPROFILE:""
 }
 
 export default API_ENDPOINTS
